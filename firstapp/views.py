fe4a616ac4c4a95d0cda42f35987baf2ac9008db
spowers0409 <spowers0409@gmail.com>
2024-06-04 07:06:20 -0500

    added render feautre

--- firstapp/views.py
+++ firstapp/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse
 # This code tells django we have a web page or a view that takes
 # as a parameter a request and returns a HttpResponse object
 # The HttpResponse object is given an HTML test to give back to the calling browser.
-def hello(request):
-    text = """<h1>Hello World!</h1>"""
-    return HttpResponse(text)
+# def hello(request):
+#     text = """<h1>Hello World!</h1>"""
+#     return HttpResponse(text)
 
-# Create your views here.
+# Here we are using a new function called render which takes the request from the browser
+# as well as atemplate and 'renders' the HTML to the browser
+def hello(request):
+    return render(request, 'firstapp/hello.html')
